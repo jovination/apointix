69cd7a8df4d0a0044982c3d626112ad31a705a8b
jovination <jovination791@gmail.com>
2024-10-31 22:48:34 +0300

    implementing dialog

--- app/components/AuthModal.tsx
+++ app/components/AuthModal.tsx
@@ -3,6 +3,7 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from 
 import Image from "next/image";
 import Logo from "@/public/logo.png"
 import GoogleIcon from "@/public/google.svg"
+import GithubIcon  from "@/public/github.svg";
 
 export function AuthModal(){
     return(
@@ -14,16 +15,42 @@ export function AuthModal(){
          </DialogTrigger>
          <DialogContent className="sm:max-w-[450px]">
             <DialogHeader >
-            <DialogTitle className="flex flex-row items-center justify-center gap-2">
-            <Image src={Logo} alt="logo" className="size-9 " />
-            <span className="text-xl font-bold text-gray-900">Apointix</span>
-            </DialogTitle>
+           
+            
+            <DialogTitle className="text-2xl font-bold text-center">
+            Create your Apointix Account
+          </DialogTitle>
+
             </DialogHeader>
-            <div className="flex flex-col mt-5 gap-3">
-                <Button className="flex items-center gap-4">
-                    <svg viewBox="0 0 533.5 544.3" xmlns="http://www.w3.org/2000/svg"><path d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z" fill="#4285f4"/><path d="M272.1 544.3c73.4 0 135.3-24.1 180.4-65.7l-87.7-68c-24.4 16.6-55.9 26-92.6 26-71 0-131.2-47.9-152.8-112.3H28.9v70.1c46.2 91.9 140.3 149.9 243.2 149.9z" fill="#34a853"/><path d="M119.3 324.3c-11.4-33.8-11.4-70.4 0-104.2V150H28.9c-38.6 76.9-38.6 167.5 0 244.4l90.4-70.1z" fill="#fbbc04"/><path d="M272.1 107.7c38.8-.6 76.3 14 104.4 40.8l77.7-77.7C405 24.6 339.7-.8 272.1 0 169.2 0 75.1 58 28.9 150l90.4 70.1c21.5-64.5 81.8-112.4 152.8-112.4z" fill="#ea4335"/></svg>
+            <div className="flex flex-col mt-5 gap-4">
+                <Button className="flex items-center gap-4" variant="outline">
+                     <Image src={GoogleIcon} alt="Google Icon" className="size-4" />
                    <span> Sign with Google </span>
                 </Button>
+                <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <span className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-xs ">
+              <span className="bg-white px-2 text-gray-500">or</span>
+            </div>
+          </div>
+                <Button className="flex items-center gap-4" variant="outline">
+                    <Image src={GithubIcon} alt="github icon"  className="size-5" />      
+                   <span> Sign with Github </span>
+                </Button>
+                <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <span className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-xs ">
+              <span className="bg-white px-2 text-gray-500">or</span>
+            </div>
+          </div>
+          <Button>
+            Continue with Email
+          </Button>
+
             </div>
 
 
